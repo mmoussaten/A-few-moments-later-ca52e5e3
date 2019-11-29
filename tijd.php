d7666f69fa3970ecd6afc244377252d41f3ab9dc
marwan <marwan0320@live.nl>
2019-11-29 08:59:36 +0100

    Uitwerking van de exercise

---
+++ tijd.php
@@ -0,0 +1,29 @@
+<?php
+
+$seconden = 0;
+$minuten = 0;
+$uren = 0;
+$dagen = 0;
+$totaal = 0;
+
+$explInput = explode(" ", $argv[1]);
+
+for ($i = 0; $i < count($explInput); $i++) {
+    $split = substr($explInput[$i], -1);
+    switch ($split) {
+        case "s":
+            $seconden = (int) $explInput[$i];
+            break;
+        case "m":
+            $minuten = (int) $explInput[$i] * 60;
+            break;
+        case "u":
+            $uren = (int) $explInput[$i] * 3600;
+            break;
+        case "d":
+            $dagen = (int) $explInput[$i] * 86400;
+            break;
+    }
+    $totaal = $seconden + $minuten + $uren + $dagen;
+}
+echo $totaal . " seconden" . PHP_EOL;
